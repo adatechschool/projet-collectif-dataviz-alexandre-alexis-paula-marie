0f4ce9c7fd9521d55574a18c64a40885e7b68130
MarieCoulay <marie.coulay@gmail.com>
2023-07-18 10:36:24 +0200

    Premier commit

---
+++ script.js
@@ -0,0 +1,37 @@
+async function planetInfo(planetName) {
+    const response = await fetch(`https://api.le-systeme-solaire.net/rest/bodies/${planetName}`);
+    const infos = await response.json();
+    console.log(infos);
+    document.body.innerHTML += `<p>Nom: ${infos.name}</p>`;
+    document.body.innerHTML += `<p>Période de révolution autour du soleil: ${infos.sideralOrbit} jours</p>`;
+    document.body.innerHTML += `<p>Période de rotation sur son axe: ${infos.sideralRotation} heures</p>`;
+    document.body.innerHTML += `<p>Rayon équatorial en kilomètres: ${infos.equaRadius} km</p>`;
+}
+
+async function moonInfo(moonName) {
+    const response = await fetch(`https://api.le-systeme-solaire.net/rest/bodies/${moonName}`);
+    const infos = await response.json();
+    console.log(infos);
+    document.body.innerHTML += `<p>Nom: ${infos.name}</p>`;
+    document.body.innerHTML += `<p>Rayon équatorial en kilomètres: ${infos.equaRadius} km</p>`;
+}
+
+async function starInfo(starName) {
+    const response = await fetch(`https://api.le-systeme-solaire.net/rest/bodies/${starName}`);
+    const infos = await response.json();
+    console.log(infos);
+    document.body.innerHTML += `<p>Nom: ${infos.name}</p>`;
+    document.body.innerHTML += `<p>Rayon équatorial en kilomètres: ${infos.equaRadius} km</p>`;
+}
+
+function afficherPlus() {
+    if (article.className == "ouvert") {
+        // afficher moins
+        article.className ="";
+        bouton.innerHTML = "Afficher plus";
+    } else {
+        // afficher plus
+        article.className = "ouvert";
+        bouton.innerHTML = "Afficher moins";
+    }
+}
